2ef38f43cd806d1b7853e8c1a72d94c031bad5fb
Rahul <computergpt987@gmail.com>
2026-08-22 20:40:54 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { Buffer } from 'buffer';
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; 
 import App from './App';
 import './index.css';
 
